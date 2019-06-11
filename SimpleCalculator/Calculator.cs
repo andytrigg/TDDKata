ba5bb0481f5f271d8deec4d3418dfca15e637e52
Andy Trigg <andy.trigg@envato.com>
2019-06-11 13:58:17 +1000

    Satify the test to validate we can deal with newlines as delimiters.

--- SimpleCalculator/Calculator.cs
+++ SimpleCalculator/Calculator.cs
@@ -5,6 +5,7 @@ namespace SimpleCalculator
         public static int Add(string numbers)
         {
             if (numbers == "") return 0;
+            numbers = numbers.Replace('\n', ',');
 
             var values = numbers.Split(',');
             var sum = 0;
