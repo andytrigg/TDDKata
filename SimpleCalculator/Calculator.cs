7d25ac91a2a3f07af3661e3afb9d3441228afe53
Andy Trigg <andy.trigg@envato.com>
2019-06-11 14:28:23 +1000

    Satisfied unit test for custom delimiters.

--- SimpleCalculator/Calculator.cs
+++ SimpleCalculator/Calculator.cs
@@ -7,9 +7,16 @@ namespace SimpleCalculator
         public static int Add(string numbers)
         {
             if (numbers == "") return 0;
-            numbers = numbers.Replace('\n', ',');
 
-            var values = numbers.Split(',');
+            var delimiter = ',';
+            if (numbers.StartsWith("//"))
+            {
+                delimiter = numbers[2];
+                numbers = numbers.Substring(4);
+            }
+            numbers = numbers.Replace('\n', delimiter);
+
+            var values = numbers.Split(delimiter);
             var sum = 0;
             foreach (var value in values)
             {
